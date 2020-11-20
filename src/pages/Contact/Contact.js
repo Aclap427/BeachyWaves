e8eb7ee0e690a525e6acdb310786b015675774cf
Amanda Claproth <amanda.claproth@gmail.com>
2020-11-20 12:19:53 -0800

    html contact

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -5,6 +5,17 @@ function Contact() {
     return (
         <div id="contact">
             <p> Contact Page</p>
+            <div id="form">
+                <form name="form" action="mailto:amandaurquidi427@gmail.com" enctype="text/plain" method="POST">
+                    <p><b>Name:</b><input TYPE="TEXT" SIZE="16" NAME="Clients Name"></input></p>
+                        <p><b>Phone:</b> <input TYPE="TEXT" SIZE="16" NAME="Clients Phone"></input></p>
+                            <p><b>Message:</b> <input TYPE="TEXT" SIZE="16" NAME="Clients Message"></input></p>
+                            <p><input TYPE="SUBMIT" VALUE="Submit"></input></p>
+
+
+                   
+                </form>
+            </div>
         </div>
     )
 };
